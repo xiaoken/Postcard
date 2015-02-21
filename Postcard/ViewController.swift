5697935a490b8fecde2b7c2a21b3c72103912019
xiaoken <xiaoken2000@gmail.com>
2015-02-21 22:09:14 +0800

    test of upgrading

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Test of Pushing to github
     }
     @IBAction func sendMailButtonPressed(sender: UIButton) {
     messageLabel.hidden=false
